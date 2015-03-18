acb35fafac55e58d83b2e0a2a2574f27a0e24f7f
shadowfax <unaputacuenta@hotmail.com>
2015-03-18 15:13:02 +0100

    Getting data

--- src/movistartv.c
+++ src/movistartv.c
@@ -1,6 +1,7 @@
 #include "sockets/socket.h"
 #include <stdio.h>
 #include <curl/curl.h>
+#include <json-c/json.h>
 
 static const char *client_profile_url =
     "http://172.26.22.23:2001/appserver/mvtv.do?action=getClientProfile";
@@ -91,40 +92,35 @@ CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch)
 	return rcode;
 }
 
-int main(int argc, char * argv[])
+json_object* fetch_json(const char *url)
 {
 	CURL *ch;												/* curl handle */
 	CURLcode rcode;                                         /* curl result code */
-	struct curl_slist *headers = NULL;                      /* http headers to send with request */
+
 	struct curl_fetch_st curl_fetch;                        /* curl fetch struct */
 	struct curl_fetch_st *cf = &curl_fetch;                 /* pointer to fetch struct */
+	struct curl_slist *headers = NULL;                      /* http headers to send with request */
 
-#ifdef WIN32
-	WSADATA wsaData;
-
-	if (0 != WSAStartup(MAKEWORD(2,2), &wsaData)) {
-		printf("Failed. Error Code : %d", WSAGetLastError());
-		return 1;
-	}
-#endif
+	struct json_object *json;                                      /* json post body */
+	enum json_tokener_error jerr = json_tokener_success;    /* json parse error */
 
 	/* init curl handle */
-    if ((ch = curl_easy_init()) == NULL) {
-        /* log error */
-        fprintf(stderr, "ERROR: Failed to create curl handle in fetch_session");
-        /* return error */
-        return 1;
-    }
+	if ((ch = curl_easy_init()) == NULL) {
+		/* log error */
+		fprintf(stderr, "ERROR: Failed to create curl handle in fetch_session");
+		/* return error */
+		return NULL;
+	}
 
 	/* set content type */
-    headers = curl_slist_append(headers, "Accept: application/json");
+	headers = curl_slist_append(headers, "Accept: application/json");
 
 	/* fetch page and capture return code */
-	rcode = curl_fetch_url(ch, platform_profile_url, cf);
+	rcode = curl_fetch_url(ch, url, cf);
 
 	/* cleanup curl handle */
 	curl_easy_cleanup(ch);
- 
+
 	/* free headers */
 	curl_slist_free_all(headers);
 
@@ -133,25 +129,90 @@ int main(int argc, char * argv[])
 		/* log error */
 		fprintf(stderr, "ERROR: Failed to fetch url (%s) - curl said: %s", platform_profile_url, curl_easy_strerror(rcode));
 		/* return error */
-		return 2;
-    }
+		return NULL;
+	}
 
 	/* check payload */
 	if (cf->payload != NULL) {
 		/* print result */
-		printf("CURL Returned: \n%s\n", cf->payload);
+		//printf("CURL Returned: \n%s\n", cf->payload);
 		/* parse return */
-		//json = json_tokener_parse_verbose(cf->payload, &jerr);
-        /* free payload */
+		json = json_tokener_parse_verbose(cf->payload, &jerr);
+		/* free payload */
 		free(cf->payload);
-	} else {
+	}
+	else {
 		/* error */
 		fprintf(stderr, "ERROR: Failed to populate payload");
 		/* free payload */
 		free(cf->payload);
 		/* return */
-		return 3;
-    }
+		return NULL;
+	}
+
+	/* check error */
+	if (jerr != json_tokener_success) {
+		/* error */
+		fprintf(stderr, "ERROR: Failed to parse json string");
+		/* free json object */
+		json_object_put(json);
+		/* return */
+		return NULL;
+	}
+
+	int json_result_code = json_object_get_int(json, "resultCode");
+	if (json_result_code != 0) {
+		json_object * result_text = json_object_object_get(json, "resultText");
+		const char * json_result_text = json_object_get_string(result_text);
+		/* error */
+		fprintf(stderr, "ERROR: json said: %s", json_result_text);
+		/* free json object */
+		json_object_put(result_text);
+		json_object_put(json);
+		/* return */
+		return NULL;
+	}
+	
+	return json;
+}
+
+int main(int argc, char * argv[])
+{
+	struct json_object *json_client_profile;
+	struct json_object *json_platform_profile;
+#ifdef WIN32
+	WSADATA wsaData;
+
+	if (0 != WSAStartup(MAKEWORD(2,2), &wsaData)) {
+		/* error */
+		fprintf(stderr, "Failed. Error Code : %d", WSAGetLastError());
+		return 1;
+	}
+#endif
+
+	json_client_profile = fetch_json(client_profile_url);
+	if (json_client_profile == NULL) {
+		/* error */
+		fprintf(stderr, "ERROR: Failed to get the client profile");
+#ifdef WIN32
+		WSACleanup();
+#endif
+		return 1;
+	}
+
+	json_platform_profile = fetch_json(platform_profile_url);
+	if (json_platform_profile == NULL) {
+		/* error */
+		fprintf(stderr, "ERROR: Failed to get the platform profile");
+#ifdef WIN32
+		WSACleanup();
+#endif
+		return 1;
+	}
+
+	/* debugging */
+	/*printf("\n\nClient Profile JSON: %s\n", json_object_to_json_string(json_client_profile));*/
+	/*printf("\n\nPlatform Profile JSON: %s\n", json_object_to_json_string(json_platform_profile));*/
 
 #ifdef WIN32
 	WSACleanup();
